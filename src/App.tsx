2cd9c668670b21b691f7f4a143b512dd60aff42e
samLynux <samuelbisnes@gmail.com>
2022-02-16 11:55:25 +0700

    38

--- src/App.tsx
+++ src/App.tsx
@@ -3,46 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Menu from './components/Menu';
+import Dashboard from './pages/dashboard';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Users from './pages/Users';
+
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <BrowserRouter>
+        <Nav/>
 
-      <div className="container-fluid">
-        <div className="row">
-          <Menu/>
-
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        
-
-
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr> 
-                </tbody>
-              </table>
-            </div>
-          </main>
+        <div className="container-fluid">
+          <div className="row">
+            <Menu/>
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            
+              <Route path='/' exact component={Dashboard}/>
+              <Route path='/users' component={Users}/>
+              
+            </main>
+          </div>
         </div>
-      </div>
-
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/register.tsx
@@ -0,0 +1,14 @@
+import React,{ Component } from "react";
+
+
+class Register extends Component {
+    render(){
+        return (
+            <div >
+              Dashboard
+            </div>
+        )
+    }
+}
+
+export default Register;
