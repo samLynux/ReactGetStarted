f340323d79b0ecc86a95432d51232f11c887137c
samLynux <samuelbisnes@gmail.com>
2022-02-16 09:53:09 +0700

    2

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Menu from './components/Menu';
+import Dashboard from './pages/Dashboard';
+
 
 function App() {
   return (
@@ -12,33 +14,8 @@ function App() {
       <div className="container-fluid">
         <div className="row">
           <Menu/>
-
+            <Dashboard/>
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        
-
-
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr> 
-                </tbody>
-              </table>
-            </div>
           </main>
         </div>
       </div>
