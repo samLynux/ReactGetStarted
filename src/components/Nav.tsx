3c7dd6cccf0812de3d6e5b9dc35beaf570c735a7
samLynux <samuelbisnes@gmail.com>
2022-02-16 15:52:52 +0700

    45

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,31 @@
-import React,  { Component } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const Nav = () =>{
+
+    const [user, setUser]= useState({
+        first_name: 'x'
+    });
 
 
-class Nav extends Component{
-    render(){
-        return ( 
-        <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    useEffect(() =>{
+       ( async () =>{
+            const {data} = await axios.get('http://localhost:8000/api/user',{withCredentials: true});
+
+            setUser(data);
+        })();
+
+    }, []);
+        
+    return ( 
+        <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
             <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-            <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"/>
-            <div className="navbar-nav">
-            <div className="nav-item text-nowrap">
-                <a className="nav-link px-3" href="#">Sign out</a>
-            </div>
-            </div>
-        </header>);
-    }
+            <ul className="my-2 my-md-0 mr-md-3">
+                <a className="p-2 text-white text-decoration-none" href="#" >{user?.first_name}</a>
+                <a className="p-2 text-white text-decoration-none"  href="#" >Sign Out</a>
+            </ul>
+        </nav>);
+    
 }
 
 export default Nav;
