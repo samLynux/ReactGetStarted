3cad9422a465362931157c1b54d4b04a30112aaf
samLynux <samuelbisnes@gmail.com>
2022-02-17 13:34:13 +0700

    51

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -7,18 +7,32 @@ import { User } from "../../models/user";
 const Users = () => {
 
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(0);
 
   useEffect(() => {
     (
       async () =>{
-        const {data} = await axios.get('users');
+        const {data} = await axios.get(`users?page=${page}`);
 
         setUsers(data.data);
+        setLastPage(data.meta.lastpage);
         
       }
     )()
-  }, [])
+  }, [page])
 
+  const next = () => {
+    if(page < lastPage){
+      setPage(page + 1);
+    }
+  }
+
+  const prev = () => {
+    if(page > 1){
+      setPage(page - 1);
+    }
+  }
   
         return (
           <Wrapper>
@@ -48,6 +62,16 @@ const Users = () => {
                 </tbody>
               </table>
             </div>
+            <nav>
+              <ul className="pagination">
+                <li className="page-item">
+                  <a href="#" className="page-link" onClick={prev}>Previous</a>
+                </li>
+                <li className="page-item">
+                  <a href="#" className="page-link" onClick={next}>Next</a>
+                </li>
+              </ul>
+            </nav>
           </Wrapper>
         )
     
